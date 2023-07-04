11c060d7b12467af11c5a5f1d8eb8d92ac7e0d53
DLMazk <daniel.patrick.litterst@hs-furtwangen.de>
2023-07-04 19:02:32 +0200

    Lückentext Poin&Click fertiggestellt

--- Minigames/Lueckentext/lueckentext.js
+++ Minigames/Lueckentext/lueckentext.js
@@ -1,98 +1,103 @@
 "use strict";
-var cloze;
-(function (cloze) {
-    let text = document.getElementById("text");
-    // let inputArr: TextIpt[] = [];
-    let inputMix = [];
-    let buttDiv = document.getElementById("backButton");
-    let backButton = document.createElement("button");
-    backButton.innerHTML = "Startseite";
-    // Back to the main game/ reload page
-    backButton.addEventListener("click", returnToStart);
-    buttDiv.append(backButton);
-    let jsonFile = new URLSearchParams(window.location.search).get("json");
-    getData(jsonFile);
-    async function getData(_url) {
-        let response = await fetch(_url);
-        let data = await response.json();
-        // inputArr = data;
-        inputMix = data;
-        for (let i = 0; i < data.length; i++) {
-            text.innerHTML += data[i].text;
-            text.append(createInput(data[i].input, i));
-            text.innerHTML += data[i].text_after;
-        }
-        showAllWords(inputMix);
-        addClickEvents();
+let text = document.getElementById("text");
+// let inputArr: TextIpt[] = [];
+let inputMix = [];
+let buttDiv = document.getElementById("backButton");
+let backButton = document.createElement("button");
+backButton.innerHTML = "Startseite";
+// Back to the main game/ reload page
+backButton.addEventListener("click", returnToStart);
+buttDiv.append(backButton);
+let jsonFile = new URLSearchParams(window.location.search).get("json");
+getData(jsonFile);
+async function getData(_url) {
+    let response = await fetch(_url);
+    let data = await response.json();
+    // inputArr = data;
+    inputMix = data;
+    for (let i = 0; i < data.length; i++) {
+        text.innerHTML += data[i].text;
+        text.append(createInput(data[i].input, i));
+        text.innerHTML += data[i].text_after;
     }
-    function createInput(_dataInput, _inputCount) {
-        // get size of word and fit input field to it
-        let inputSpan = document.createElement("span");
-        inputSpan.setAttribute("id", _dataInput);
-        inputSpan.setAttribute("class", "input");
-        // inputSpan.addEventListener("click", () => { checkInput(_dataInput); });
-        let output = "";
-        for (let i = 0; i < _dataInput.length; i++) {
-            output += '_';
-        }
-        inputSpan.innerHTML = output;
-        // text.appendChild(inputSpan);
-        return inputSpan;
+    showAllWords(inputMix);
+    addClickEvents();
+}
+function createInput(_dataInput, _inputCount) {
+    // get size of word and fit input field to it
+    let inputSpan = document.createElement("span");
+    inputSpan.setAttribute("class", _dataInput.toString() + " input");
+    // inputSpan.setAttribute("class", "input");
+    // inputSpan.addEventListener("click", () => { checkInput(_dataInput); });
+    let output = "";
+    for (let i = 0; i < _dataInput.length; i++) {
+        output += '_';
     }
-    function showAllWords(allWords) {
-        let wordsDiv = document.getElementById("words");
-        wordsDiv.setAttribute("id", "allWords");
-        let wordsRandArray = allWords;
-        wordsRandArray.sort(() => .5 - Math.random());
-        for (let i = 0; i < wordsRandArray.length; i++) {
-            let thisWord = document.createElement("p");
-            if (i == 0) {
-                thisWord.innerHTML = wordsRandArray[i].input;
-            }
-            else {
-                thisWord.innerHTML = wordsRandArray[i].input;
-            }
-            thisWord.addEventListener("click", () => checkInput(wordsRandArray[i].input, false));
-            wordsDiv.appendChild(thisWord);
-        }
-    }
-    function addClickEvents() {
-        let spans = document.querySelectorAll("span");
-        spans.forEach(spans => {
-            spans.addEventListener("click", () => checkInput(spans.id, true));
-        });
-    }
-    let string1 = "leer";
-    let string2 = "leer";
-    function checkInput(_dataInput, isInText) {
-        // console.log("Checking input");
-        // console.log(_dataInput);
-        if (isInText) {
-            string1 = _dataInput;
+    inputSpan.innerHTML = output;
+    // text.appendChild(inputSpan);
+    return inputSpan;
+}
+function showAllWords(allWords) {
+    let wordsDiv = document.getElementById("words");
+    wordsDiv.setAttribute("id", "allWords");
+    let wordsRandArray = allWords;
+    wordsRandArray.sort(() => .5 - Math.random());
+    for (let i = 0; i < wordsRandArray.length; i++) {
+        let thisWord = document.createElement("p");
+        if (i == 0) {
+            thisWord.innerHTML = wordsRandArray[i].input;
         }
         else {
-            string2 = _dataInput;
+            thisWord.innerHTML = wordsRandArray[i].input;
         }
-        console.log("string1: " + string1);
-        console.log("string2: " + string2);
-        if (string1 == string2) {
-            let inputSpan = document.getElementById(string1);
-            inputSpan.innerHTML = string1;
-            inputSpan.classList.add("bold");
-            inputSpan.classList.remove("input");
-            string1 = "leer";
-            string2 = "leer";
-        }
-        if (string2 != "leer") {
-            string1 = "leer";
-            string2 = "leer";
-            // console.log("string1: " + string1);
-            // console.log("string2: " + string2);
-        }
+        thisWord.setAttribute("class", wordsRandArray[i].input);
+        thisWord.addEventListener("click", () => checkInput(wordsRandArray[i].input, false));
+        wordsDiv.appendChild(thisWord);
     }
-    // reload the page (to restart the game)
-    function returnToStart() {
-        window.open("../Startseite/Startseite.html", "_self");
+}
+function addClickEvents() {
+    let spans = document.querySelectorAll("span");
+    spans.forEach(spans => {
+        let thisSpan = spans.classList.toString();
+        let substringToRemove = " input";
+        let resultString = thisSpan.replace(substringToRemove, "");
+        spans.addEventListener("click", () => checkInput(resultString, true));
+    });
+}
+let string1 = "leer";
+let string2 = "leer";
+let counter = 0;
+function checkInput(_dataInput, isInText) {
+    // console.log("Checking input");
+    // console.log(_dataInput);
+    if (isInText) {
+        string1 = _dataInput;
     }
-})(cloze || (cloze = {}));
+    else {
+        string2 = _dataInput;
+    }
+    console.log("string1: " + string1);
+    console.log("string2: " + string2);
+    if (string1 == string2) {
+        console.log("Correct");
+        let inputSpan = document.getElementsByClassName(string1)[0];
+        inputSpan.innerHTML = string1;
+        inputSpan.classList.add("bold");
+        inputSpan.classList.remove("input");
+        counter++;
+        string1 = "leer";
+        string2 = "leer";
+    }
+    if (counter == inputMix.length) {
+        console.log("Youre done");
+    }
+    if (string1 != "leer" && string2 != "leer") {
+        string1 = "leer";
+        string2 = "leer";
+    }
+}
+// reload the page (to restart the game)
+function returnToStart() {
+    window.open("../Startseite/Startseite.html", "_self");
+}
 //# sourceMappingURL=lueckentext.js.map
--- Minigames/Lueckentext/lueckentext.ts
+++ Minigames/Lueckentext/lueckentext.ts
@@ -1,132 +1,138 @@
-namespace cloze {
-    interface TextIpt {
+interface TextIpt {
 
-        text: string;
-        input: string;
-        text_after: string;
+    text: string;
+    input: string;
+    text_after: string;
 
-    }
+}
 
-    let text: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("text");
+let text: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("text");
 
-    // let inputArr: TextIpt[] = [];
-    let inputMix: TextIpt[] = [];
+// let inputArr: TextIpt[] = [];
+let inputMix: TextIpt[] = [];
 
-    let buttDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("backButton")
-    let backButton: HTMLButtonElement = <HTMLButtonElement>document.createElement("button");
-    backButton.innerHTML = "Startseite";
-    // Back to the main game/ reload page
-    backButton.addEventListener("click", returnToStart);
-    buttDiv.append(backButton);
+let buttDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("backButton")
+let backButton: HTMLButtonElement = <HTMLButtonElement>document.createElement("button");
+backButton.innerHTML = "Startseite";
+// Back to the main game/ reload page
+backButton.addEventListener("click", returnToStart);
+buttDiv.append(backButton);
 
-    let jsonFile: RequestInfo = new URLSearchParams(window.location.search).get("json");
-    getData(jsonFile);
+let jsonFile: RequestInfo = new URLSearchParams(window.location.search).get("json");
+getData(jsonFile);
 
-    async function getData(_url: RequestInfo): Promise<void> {
+async function getData(_url: RequestInfo): Promise<void> {
 
-        let response: Response = await fetch(_url);
-        let data: TextIpt[] = await response.json();
-        // inputArr = data;
-        inputMix = data;
+    let response: Response = await fetch(_url);
+    let data: TextIpt[] = await response.json();
+    // inputArr = data;
+    inputMix = data;
 
-        for (let i: number = 0; i < data.length; i++) {
+    for (let i: number = 0; i < data.length; i++) {
 
-            text.innerHTML += data[i].text;
-            text.append(createInput(data[i].input, i));
-            text.innerHTML += data[i].text_after;
-
-        }
-        showAllWords(inputMix);
-        addClickEvents();
-    }
-
-    function createInput(_dataInput: string, _inputCount: number): HTMLSpanElement {
-
-        // get size of word and fit input field to it
-        let inputSpan: HTMLSpanElement = <HTMLSpanElement>document.createElement("span");
-        inputSpan.setAttribute("id", _dataInput);
-        inputSpan.setAttribute("class", "input");
-        // inputSpan.addEventListener("click", () => { checkInput(_dataInput); });
-        let output: string = "";
-        for (let i = 0; i < _dataInput.length; i++) {
-            output += '_';
-        }
-        inputSpan.innerHTML = output;
-
-        // text.appendChild(inputSpan);
-
-        return inputSpan;
+        text.innerHTML += data[i].text;
+        text.append(createInput(data[i].input, i));
+        text.innerHTML += data[i].text_after;
 
     }
+    showAllWords(inputMix);
+    addClickEvents();
+}
 
-    function showAllWords(allWords: TextIpt[]): void {
+function createInput(_dataInput: string, _inputCount: number): HTMLSpanElement {
 
-        let wordsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("words");
-        wordsDiv.setAttribute("id", "allWords");
-
-        let wordsRandArray: TextIpt[] = allWords;
-        wordsRandArray.sort(() => .5 - Math.random());
-
-        for (let i = 0; i < wordsRandArray.length; i++) {
-            let thisWord: HTMLParagraphElement = <HTMLParagraphElement>document.createElement("p");
-            if (i == 0) {
-                thisWord.innerHTML = wordsRandArray[i].input;
-            } else {
-                thisWord.innerHTML = wordsRandArray[i].input;
-            }
-            thisWord.addEventListener("click", () => checkInput(wordsRandArray[i].input, false));
-            wordsDiv.appendChild(thisWord);
-        }
+    // get size of word and fit input field to it
+    let inputSpan: HTMLSpanElement = <HTMLSpanElement>document.createElement("span");
+    inputSpan.setAttribute("class", _dataInput.toString() + " input");
+    // inputSpan.setAttribute("class", "input");
+    // inputSpan.addEventListener("click", () => { checkInput(_dataInput); });
+    let output: string = "";
+    for (let i = 0; i < _dataInput.length; i++) {
+        output += '_';
     }
+    inputSpan.innerHTML = output;
 
-    function addClickEvents(): void {
+    // text.appendChild(inputSpan);
 
-        let spans = document.querySelectorAll("span");
-        spans.forEach(spans => {
-            spans.addEventListener("click", () => checkInput(spans.id, true));
-        }
-        );
-    }
+    return inputSpan;
 
+}
 
-    let string1: string = "leer";
-    let string2: string = "leer";
+function showAllWords(allWords: TextIpt[]): void {
 
-    function checkInput(_dataInput: string, isInText: boolean): void {
+    let wordsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("words");
+    wordsDiv.setAttribute("id", "allWords");
 
-        // console.log("Checking input");
-        // console.log(_dataInput);
-        if (isInText) {
-            string1 = _dataInput;
+    let wordsRandArray: TextIpt[] = allWords;
+    wordsRandArray.sort(() => .5 - Math.random());
+
+    for (let i = 0; i < wordsRandArray.length; i++) {
+        let thisWord: HTMLParagraphElement = <HTMLParagraphElement>document.createElement("p");
+        if (i == 0) {
+            thisWord.innerHTML = wordsRandArray[i].input;
         } else {
-            string2 = _dataInput;
+            thisWord.innerHTML = wordsRandArray[i].input;
         }
+        thisWord.setAttribute("class", wordsRandArray[i].input);
+        thisWord.addEventListener("click", () => checkInput(wordsRandArray[i].input, false));
+        wordsDiv.appendChild(thisWord);
+    }
+}
 
-        console.log("string1: " + string1);
-        console.log("string2: " + string2);
+function addClickEvents(): void {
 
-        if (string1 == string2) {
-            let inputSpan: HTMLSpanElement = <HTMLSpanElement>document.getElementById(string1);
-            inputSpan.innerHTML = string1;
-            inputSpan.classList.add("bold");
-            inputSpan.classList.remove("input");
+    let spans = document.querySelectorAll("span");
+    spans.forEach(spans => {
+        let thisSpan: string = spans.classList.toString();
+        let substringToRemove: string = " input";
+        let resultString: string = thisSpan.replace(substringToRemove, "");
+        spans.addEventListener("click", () => checkInput(resultString, true));
+    }
+    );
+}
 
-            string1 = "leer";
-            string2 = "leer";
-        }
 
-        if (string2 != "leer") {
-            string1 = "leer";
-            string2 = "leer";
-            // console.log("string1: " + string1);
-            // console.log("string2: " + string2);
-        }
+let string1: string = "leer";
+let string2: string = "leer";
+let counter: number = 0;
+
+function checkInput(_dataInput: string, isInText: boolean): void {
+
+    // console.log("Checking input");
+    // console.log(_dataInput);
+    if (isInText) {
+        string1 = _dataInput;
+    } else {
+        string2 = _dataInput;
     }
 
-    // reload the page (to restart the game)
-    function returnToStart(): void {
+    console.log("string1: " + string1);
+    console.log("string2: " + string2);
 
-        window.open("../Startseite/Startseite.html", "_self");
-
+    if (string1 == string2) {
+        console.log("Correct");
+        let inputSpan = document.getElementsByClassName(string1)[0];
+        inputSpan.innerHTML = string1;
+        inputSpan.classList.add("bold");
+        inputSpan.classList.remove("input");
+        counter++;
+        string1 = "leer";
+        string2 = "leer";
     }
+
+    if (counter == inputMix.length) {
+        console.log("Youre done");
+    }
+
+    if (string1 != "leer" && string2 != "leer") {
+        string1 = "leer";
+        string2 = "leer";
+    }
+}
+
+// reload the page (to restart the game)
+function returnToStart(): void {
+
+    window.open("../Startseite/Startseite.html", "_self");
+
 }
